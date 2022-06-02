54c36d52362a3f1a836f5709c4016fd6b6611d60
conecta-tech-app <106754470+conecta-tech-app@users.noreply.github.com>
2022-06-02 16:23:58 -0300

    Add files via upload

---
+++ Game/js/questions.js
@@ -0,0 +1,147 @@
+// creating an array and passing the number, questions, options, and answers
+let listQuestions = [
+    {
+    numb: 1,
+    question: "A demanda é muito importante para qualquer organização, pois possibilita o planejamento e o dimensionamento de todas as etapas envolvidas na cadeia de suprimentos.",
+    problem: "O que demonstra um bom planejamento de demanda?",
+    answer: "O volume máximo de produtos ou serviços que uma empresa pode comercializar",
+    options: [
+      "O volume mínimo apenas de produtos que sua empresa comercializa",
+      "O volume mínimo de produtos e serviços que sua empresa comercializa",
+      "O volume médio de produtos e serviços de comercio da sua empresa",
+      "O volume máximo de produtos ou serviços que uma empresa pode comercializar"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Qual alternativa descreve o que é Sazonalidade?",
+    problem: "A sazonalidade afeta o estoque e caixa de várias empresas.",
+    answer: "Estados eventuais que vendem mais de determinada mercadoria",
+    options: [
+      "O volume de compras em estoque no final do mês",
+      "Produtos que são vendidos em épocas especificas durante o ano",
+      "Periodos eventuais que vendem mais de determinada mercadoria",
+      "Produtos que são vendidos em qualquer época do ano"
+    ]
+  },
+    {
+    numb: 3,
+    question: " Abaixo marque a opção que os descrevam:",
+    problem: "Existem elementos principais nos modelos de analises de demanda. .",
+    answer: "Tendencia, nível, sazonal e alectória.",
+    options: [
+      "Tendencia, nível, sazonal e alectória.",
+      "Inflação, Tendencia, aleatório e concorrência.",
+      "Concorrência, Nível, sazonal e tendencia.",
+      "Aleatória, concorrência, nível e sazonal."
+    ]
+  },
+    {
+    numb: 4,
+    question: "Sabe-se que a produção anda lado a lado com a Demanda e é importante que aja um equilíbrio entre elas.",
+    problem: "Quando a produção se torna drasticamente maior que a demanda, pode ocorrer:",
+    answer: "Um aumento de estoque.",
+    options: [
+      "Um aumento de estoque.",
+      "Um aumento de preço sob a mercadoria.",
+      "Um nível baixo de estoque.",
+      "Aumento de ativos no caixa."
+    ]
+  },
+    {
+    numb: 5,
+    question: "O que não é provável acontecer? ",
+    problem: "Suponha-se que a demanda esteja maior que a produção. ",
+    answer: "Aumento da oferta que causa uma queda no preço da mercadoria.",
+    options: [
+      "Um aumento no tempo de atendimento referente a pedido de clientes.",
+      "Um nível mais baixo de estoque.",
+      "Aumento da oferta que causa uma queda no preço da mercadoria.",
+      "Um problema no planejamento de estoque e produção."
+    ]
+  },
+    {
+    numb: 6,
+    question: "Assinale a alternativa que se refere ao que pode acontecer em casos de ruptura:",
+    problem: "A ruptura de estoque é um fator negativo para empresas, gerando situações desagradáveis.",
+    answer: "Perda de vendas para a concorrência, por não ter o produto desejável pelo cliente.",
+    options: [
+      "Perda de vendas para a concorrência, por não ter o produto desejável pelo cliente.",
+      "Gera mais economia por ter menos produtos estocados.",
+      "Aumento de ativos no caixa.",
+      "Melhorias no espaço."
+    ]
+  }, 
+  {
+    numb: 7,
+    question: "Sobre a ruptura, responda:",
+    problem: "Quais das alternativas é uma causa de ruptura?",
+    answer: "Analise de dados ineficiente.",
+    options: [
+      "Reposição eficiente de estoque.",
+      "Analise de dados ineficiente.",
+      "Conhecer seus clientes.",
+      "Entregas rápidas dos fornecedores."
+    ]
+  },
+    {
+    numb: 8,
+    question: "Assinale a alternativa que corresponde a alguns de fatores convenientes: ",
+    problem: "Caso queira um caixa cheio, é primordial que exista uma previsão útil referente a demandas.",
+    answer: "Tempo de reabastecimento, Demanda passada e ter preços planejados.",
+    options: [
+      "Demanda passada e intuição sobre o reabastecimento.",
+      "Demanda passada, estado da economia e marketing digital.",
+      "Propaganda, descontos não planejados sob preços e estado da economia.",
+      "Tempo de reabastecimento, Demanda passada e ter preços planejados."
+    ]
+  },
+    {
+    numb: 9,
+    question: "Marque a alternativa que mostre uma das consequências:",
+    problem: "Sobre a ruptura de estoque.",
+    answer: "inibição de vendas.",
+    options: [
+      "má organização.",
+      "inibição de vendas.",
+      "maior número de vendas.",
+      "lucratividade."
+    ]
+  },
+    {
+    numb: 10,
+    question: "Por que João tem essa dor?",
+    problem: "João é responsável pelo setor de vendas de um supermercado e sente a dor quando falta ou sobra de alguma mercadoria.",
+    answer: "Pois a inibição de vendas pode surgir pela falta e o excesso de produtos.",
+    options: [
+      "Pois um estoque bem gerenciado diminui as vendas.",
+      "Pois a inibição de vendas pode surgir pela falta e o excesso de produtos.",
+      "Pois João não confia em seu colaborador que gerencia o estoque.",
+      "Pois João precisa que sobre produtos para que resulte em um caixa maior."
+    ]
+  },
+    {
+    numb: 11,
+    question: "O que ocasiona:",
+    problem: "Sabemos que o período que mais são vendidos acondicionados são nos meses de dezembro, janeiro e fevereiro, caso o fornecedor tenha falta de insumos e só consiga entregar as peças depois do prazo.",
+    answer: "Aumento da oferta que causa uma queda no preço da mercadoria.",
+    options: [
+      "Aumento de procura em concorrentes.",
+      "Aumento de ativos no caixa.",
+      "Lucratividade.",
+      "Diminuição de procura em concorrente."
+    ]
+  },
+    {
+    numb: 12,
+    question: "Quais das alternativas se aplicam as análises antes da tomada de decisão?",
+    problem: "O período com maior quantidade de itens vendidos em uma papelaria é no tempo de volta as aulas.",
+    answer: "Analisar períodos passados e formar cálculos específicos e complexos.",
+    options: [
+      "Comprar sempre  a mais para não faltar.",
+      "Comrpsr sempre menos  para não sobrar.",
+      "Analisar as quantidades no ano anterior e comprar igual.",
+      "Analisar períodos passados e formar cálculos específicos e complexos."
+    ]
+  } 
+];
